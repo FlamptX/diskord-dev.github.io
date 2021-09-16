a47818e417d29c48b89060bcc5310c66750c42f3
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-09-17 00:37:06 +0500

    Add dark theme

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-// empty for now
+var currentTheme = 'dark';
+
+function changeTheme() {
+    body = document.getElementsByTagName('body')[0]
+    mainInfo = document.getElementById('mainInfo')
+    icon = document.getElementById('changeTheme')
+
+    if(currentTheme === 'light'){
+        body.style.color = 'white'
+        mainInfo.style.backgroundColor = '#2f3136'
+        currentTheme = 'dark'
+        icon.innerHTML = 'brightness_5'
+        return;
+    }
+    else if(currentTheme === 'dark'){
+        body.style.color = 'black'
+        mainInfo.style.backgroundColor = 'white'
+        currentTheme = 'light'
+        icon.innerHTML = 'brightness_2'
+        return;
+    }
+}
