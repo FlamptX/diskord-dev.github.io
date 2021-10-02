5a14713f77b89b2d71de01c59ced044a5b37796c
FlamptX <dino.shark2006@gmail.com>
2021-10-02 20:42:16 +0200

    Added more darker feeling to dark theme and improved js code a bit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 var currentTheme = 'dark';
 
 function changeTheme() {
-    body = document.getElementsByTagName('body')[0]
+    body = document.body
     mainInfo = document.getElementById('mainInfo')
+    navbar = document.getElementById("_navbar")
+    footer = document.getElementById("_footer")
     icon = document.getElementById('changeTheme')
 
     if(currentTheme === 'light'){
         body.style.color = 'white'
+
+        navbar.style.backgroundColor = "#111863"
+        footer.style.backgroundColor = "#111863"
+        document.body.style.backgroundColor = "#111863"
+        
         mainInfo.style.backgroundColor = '#2f3136'
         currentTheme = 'dark'
         icon.innerHTML = 'brightness_5'
-        return;
     }
     else if(currentTheme === 'dark'){
         body.style.color = 'black'
+
+        navbar.style.backgroundColor = "#5865f2"
+        footer.style.backgroundColor = "#5865f2"
+        document.body.style.backgroundColor = "#5865f2"
+
         mainInfo.style.backgroundColor = 'white'
         currentTheme = 'light'
         icon.innerHTML = 'brightness_2'
-        return;
     }
 }
